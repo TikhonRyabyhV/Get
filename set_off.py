76f07a22d081d29318ae9133fc4f9f9e460e7cca
Тихон Рябых <riabykh.tv@phystech.edu>
2023-03-23 09:45:10 +0300

    file for system cleaning

---
+++ set_off.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode( GPIO.BCM )
+leds = [ 21, 20, 16, 12, 7, 8, 25, 24 ]
+
+
+GPIO.setup( leds, GPIO.OUT )
+
+GPIO.output( leds, 0 )
+
+GPIO.cleanup()
+print( "All done" )
