4f22963330fc4f241f0135a7561d6a9e476b415f
Тихон Рябых <riabykh.tv@phystech.edu>
2023-04-06 09:53:09 +0300

    simple adc

---
+++ 5-1-adc-simple.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode( GPIO.BCM )
+
+dac = [ 26, 19, 13, 6, 5, 11, 9, 10 ]
+GPIO.setup( dac, GPIO.OUT )
+
+comp = 4
+troyka = 17
+GPIO.setup( comp, GPIO.IN )
+GPIO.setup( troyka, GPIO.OUT, initial = 0 )
+
+def decimal_2_binary ( value ):
+	return [ int (bit) for bit in bin( value )[2:].zfill(8) ]
+
+def adc ( dac, comp ):
+	for x in range( 0, 256 ):
+		GPIO.output( dac, decimal_2_binary( x ) )
+		time.sleep(0.008)
+		if( GPIO.input( comp ) == 0 ):
+			GPIO.output( dac, 0 )
+			return x
+	return 255;
+try:
+	while( True ):
+		time.sleep(0.008)
+		number = adc( dac, comp )
+		voltage = number / 256 * 3.3
+		print( "Number  " + str(number) + ", expected voltage:" + "{:.4f}".format( voltage ) + "V" )
+		time.sleep( 0.5 )
+
+finally:
+	GPIO.output( dac, 0 )
+	GPIO.output( troyka, 0 )
+	GPIO.cleanup()
