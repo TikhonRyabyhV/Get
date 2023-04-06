225f4057db14587084f81ae0009b0697a97df4e9
Тихон Рябых <riabykh.tv@phystech.edu>
2023-04-06 10:16:17 +0300

    normal adc

---
+++ 5-2-adc-sar.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode( GPIO.BCM )
+
+dac = [ 26, 19, 13, 6, 5, 11, 9, 10 ]
+GPIO.setup( dac, GPIO.OUT )
+
+comp = 4
+troyka = 17
+GPIO.setup( comp, GPIO.IN )
+GPIO.setup( troyka, GPIO.OUT, initial = 0 )
+
+def decimal_2_binary ( value ):
+	return [ int (bit) for bit in bin( value )[2:].zfill(8) ]
+
+def adc ( dac, comp ):
+	result = [ 0, 0 ]
+	for i in range( 8 ):
+		result[0] += 2**( 7 - i )
+		GPIO.output( dac, decimal_2_binary( result[0] ) )
+		time.sleep(0.008)
+		if( GPIO.input( comp ) == 0 ):
+			result[0] -= 2**( 7 - i )
+	GPIO.output( dac, 0 )
+	return result[0];
+try:
+	while( True ):
+		time.sleep(0.008)
+		number = adc( dac, comp )
+		voltage = number / 256 * 3.3
+		print( "Number  " + str(number) + ", expected voltage:" + "{:.4f}".format( voltage ) + "V" )
+		time.sleep( 0.5 )
+
+finally:
+	GPIO.output( dac, 0 )
+	GPIO.output( troyka, 0 )
+	GPIO.cleanup()
